a118fe8ddb200b00688db502a18b3ff7b9dd6a57
Kawsarul Alam Chowdhury Rifat <43786423+rifatron999@users.noreply.github.com>
2025-02-08 03:56:42 +0600

    update 03

--- content.js
+++ content.js
@@ -222,21 +222,20 @@ chrome.storage.local.get("productData", function (result) {
 
                 console.log("🚀 Clicked 'Publish' button!");
             }, 1000, 15);  // Retry every 1 second for up to 15 attempts
-            //update
-            fetch("http://localhost/update.php", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ id: product.id }) // Replace 123 with the actual product ID
-            })
-            .then(response => response.json())
-            .then(data => console.log("✅ Success:", data))
-            .catch(error => console.error("❌ Error:", error));            
-            //update end 
         }, 6000);
         //publish #
-        //
+        //update
+        fetch("http://localhost/others/chrome-extention-for-facebook-marketplace-france/update.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ id: product.id }) // Replace 123 with the actual product ID
+        })
+        .then(response => response.json())
+        .then(data => console.log("✅ Success:", data))
+        .catch(error => console.error("❌ Error:", error));            
+        //update end 
         
         
 
